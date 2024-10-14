26d74d552dc46d0838d35ee6ec359433b01ffdb9
mac <adhartor@gmail.com>
2024-10-14 20:31:41 +0300

    modified some code

---
+++ lib/models/food.dart
@@ -0,0 +1,29 @@
+//food item
+class Food {
+  final String name;
+  final String description;
+  final String imagePath;
+  final double price;
+  final FoodCategory category;
+  List<Addon> availableAddons;
+
+  Food({
+    required this.name,
+    required this.description,
+    required this.imagePath,
+    required this.price,
+    required this.category,
+    required this.availableAddons,
+  });
+}
+
+// food categories
+enum FoodCategory { burgers, salads, sides, desserts, drinks }
+
+//food addons
+class Addon {
+  String name;
+  double price;
+
+  Addon({required this.price, required this.name});
+}
